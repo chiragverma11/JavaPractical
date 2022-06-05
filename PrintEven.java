4b5884a9f412728f58e42476c9e8354e17e23bb5
Chirag Verma <chiragverma1013@gmail.com>
2022-06-05 18:31:48 +0530

    just ammended the code in for loop in PrintEven.java and Renamed TransverseArray.java

--- PrintEven.java
+++ PrintEven.java
@@ -1,8 +1,8 @@
 public class PrintEven {
     public static void main(String[] args) {
         System.out.println("Even number from 1 to 100 : ");
-        for(int i=1;i<=100;i++){
-            System.out.print(i + " ");
+        for(int i=1;i<=100/2;i++){
+            System.out.print(i*2 + " ");
         }
     }
     
--- TraverseArray.java
+++ TraverseArray.java
@@ -1,12 +1,12 @@
-public class TransverseArray {
-    //there are two ways to transverse an array
+public class TraverseArray {
+    //there are two ways to traverse an array
     public static void main(String[] args) {
         int[] arr={1,2,4,8,16,28};
-        //#1 transverse using for loop
+        //#1 traverse using for loop
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-        //#2 Transverse using for each loop
+        //#2 traverse using for each loop
         System.out.print("\n");
         for (int i : arr) {
             System.out.print(i + " ");
